1c0b499b7da4ec5aa455857eece7a1b1d69d9328
sprindy <sprindy.liu@gmail.com>
2016-11-25 10:13:47 +0800

    Add init files.

---
+++ app/src/main/java/com/chinaclassa/chinaclassa/MainActivity.java
@@ -0,0 +1,176 @@
+package com.chinaclassa.chinaclassa;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import java.util.ArrayList;
+
+import android.app.Activity;
+import android.os.Handler;
+import android.support.v4.view.PagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.support.v4.view.ViewPager.OnPageChangeListener;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    private ViewPager viewPager;
+    private LinearLayout point_group;
+    private TextView image_desc;
+
+    // 图片资源id
+    private final int[] images = { R.drawable.a, R.drawable.b, R.drawable.c,
+            /*R.drawable.d, R.drawable.e*/ };
+    // 图片标题集合
+    private final String[] imageDescriptions = { "1",
+            "2", "3", /*"", ""*/ };
+
+    private ArrayList<ImageView> imageList;
+    // 上一个页面的位置
+    protected int lastPosition = 0;
+
+    // 判断是否自动滚动viewPager
+    private boolean isRunning = true;
+    private Handler handler = new Handler() {
+        public void handleMessage(android.os.Message msg) {
+            // 执行滑动到下一个页面
+            viewPager.setCurrentItem(viewPager.getCurrentItem() + 1);
+            if (isRunning) {
+                // 在发一个handler延时
+                handler.sendEmptyMessageDelayed(0, 2000);
+            }
+
+        };
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        viewPager = (ViewPager) findViewById(R.id.viewPager);
+        point_group = (LinearLayout) findViewById(R.id.point_group);
+        image_desc = (TextView) findViewById(R.id.image_desc);
+        image_desc.setText(imageDescriptions[0]);
+
+        // 初始化图片资源
+        imageList = new ArrayList<ImageView>();
+        for (int i : images) {
+            // 初始化图片资源
+            ImageView imageView = new ImageView(this);
+            imageView.setBackgroundResource(i);
+            imageList.add(imageView);
+
+            // 添加指示小点
+            ImageView point = new ImageView(this);
+            LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(3, 3);
+            params.rightMargin = 20;
+            point.setLayoutParams(params);
+            //point.setBackgroundResource(R.drawable.point_bg);
+            if (i == R.drawable.a) {
+                point.setEnabled(true);
+            } else {
+                point.setEnabled(false);
+            }
+
+            point_group.addView(point);
+        }
+
+        viewPager.setAdapter(new MyPageAdapter());
+        // 设置当前viewPager的位置
+        viewPager.setCurrentItem(Integer.MAX_VALUE / 2
+                - (Integer.MAX_VALUE / 2 % imageList.size()));
+        viewPager.setOnPageChangeListener(new OnPageChangeListener() {
+
+            @Override
+            public void onPageSelected(int position) {
+                // 页面切换后调用， position是新的页面位置
+
+                // 实现无限制循环播放
+                position %= imageList.size();
+
+                image_desc.setText(imageDescriptions[position]);
+
+                // 把当前点设置为true,将上一个点设为false
+                point_group.getChildAt(position).setEnabled(true);
+                point_group.getChildAt(lastPosition).setEnabled(false);
+                lastPosition = position;
+
+            }
+
+            @Override
+            public void onPageScrolled(int position, float positionOffset,
+                                       int positionOffsetPixels) {
+                // 页面正在滑动时间回调
+
+            }
+
+            @Override
+            public void onPageScrollStateChanged(int state) {
+                // 当pageView 状态发生改变的时候，回调
+
+            }
+        });
+
+        /**
+         * 自动循环： 1.定时器：Timer 2.开子线程：while true循环 3.ClockManger
+         * 4.用Handler发送延时信息，实现循环，最简单最方便
+         *
+         */
+
+        handler.sendEmptyMessageDelayed(0, 2000);
+
+    }
+
+    @Override
+    protected void onDestroy() {
+        // 停止滚动
+        isRunning = false;
+        super.onDestroy();
+    }
+
+    private class MyPageAdapter extends PagerAdapter {
+        // 需要实现以下四个方法
+
+        @Override
+        public int getCount() {
+            // 获得页面的总数
+            return Integer.MAX_VALUE;
+        }
+
+        @Override
+        public boolean isViewFromObject(View view, Object object) {
+            // 判断view和Object对应是否有关联关系
+            if (view == object) {
+                return true;
+            } else {
+                return false;
+            }
+        }
+
+        @Override
+        public Object instantiateItem(ViewGroup container, int position) {
+            // 获得相应位置上的view； container view的容器，其实就是viewpage自身,
+            // position: viewpager上的位置
+            // 给container添加内容
+            container.addView(imageList.get(position % imageList.size()));
+
+            return imageList.get(position % imageList.size());
+        }
+
+        @Override
+        public void destroyItem(ViewGroup container, int position, Object object) {
+            // 销毁对应位置上的Object
+            // super.destroyItem(container, position, object);
+            container.removeView((View) object);
+            object = null;
+        }
+
+    }
+
+}
